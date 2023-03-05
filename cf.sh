9c3c71cd8b0f6034d34c34b848a49f89a322925f
Sr <64494086+0x187@users.noreply.github.com>
2023-03-05 10:15:32 +0330

    Update cf.sh

--- cf.sh
+++ cf.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 curl https://raw.githubusercontent.com/0x187/chert/main/CloudflareST -o cf
-curl https://raw.githubusercontent.com/0x187/chert/main/ip -o ip
+curl https://raw.githubusercontent.com/0x187/chert/main/ip.txt -o ip
 chmod +x cf
 ./cf -n 100 -tp 2087 -tl 400 -p 20 -f ip
