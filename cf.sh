ecd34fc79005dd7437bde0f3e5ceac49432644f9
Sr <64494086+0x187@users.noreply.github.com>
2023-03-05 10:12:27 +0330

    Add files via upload

---
+++ cf.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+curl https://raw.githubusercontent.com/0x187/chert/main/CloudflareST -o cf
+curl https://raw.githubusercontent.com/0x187/chert/main/ip -o ip
+./cf -n 100 -tp 2087 -tl 400 -p 20 -f ip
