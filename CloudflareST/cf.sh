57d1d1d4c85ba20b4028bf0b4e584d2dbff0fe6b
Sr <64494086+0x187@users.noreply.github.com>
2023-03-07 11:01:02 +0330

    Update cf.sh

--- CloudflareST/cf.sh
+++ CloudflareST/cf.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-curl https://raw.githubusercontent.com/0x187/chert/main/CloudflareST -o cf
-curl https://raw.githubusercontent.com/0x187/chert/main/ip.txt -o ip
+curl -L https://github.com/0x187/chert/raw/main/CloudflareST/CloudflareST -o cf
+curl -L https://raw.githubusercontent.com/0x187/chert/main/CloudflareST/ip -o ip
 chmod +x cf
 ./cf -n 100 -tp 2087 -tl 400 -p 20 -f ip
