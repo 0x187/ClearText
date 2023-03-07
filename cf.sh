0fc9c990368807eed782d3b1154391566cdd7999
Sr <64494086+0x187@users.noreply.github.com>
2023-03-07 10:59:56 +0330

    Delete cf.sh

--- cf.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-curl https://raw.githubusercontent.com/0x187/chert/main/CloudflareST -o cf
-curl https://raw.githubusercontent.com/0x187/chert/main/ip.txt -o ip
-chmod +x cf
-./cf -n 100 -tp 2087 -tl 400 -p 20 -f ip
